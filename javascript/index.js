fa4dd0050bcdd78abf04ddc83758da6e2f4b5250
joemar-ceneza <joemar.ceneza@gmail.com>
2022-08-24 19:59:56 +0800

    function when clicked outside of the model will close the model

--- javascript/index.js
+++ javascript/index.js
@@ -2,12 +2,18 @@ const modalBtn = document.querySelector(".modal-button");
 const modal = document.querySelector(".modal");
 const closeBtn = document.querySelector(".fa-solid");
 
+const outsideModel = document.querySelector("main");
+
 modalBtn.addEventListener("click", function(){
-    modal.classList.remove("hide");
-    modalBtn.classList.add("hide");
+    modal.classList.add("show-modal");
 });
 
 closeBtn.addEventListener("click", function(){
-    modal.classList.add("hide");
-    modalBtn.classList.remove("hide");
+    modal.classList.remove("show-modal");
+});
+
+window.addEventListener("click", function(event){
+    if(event.target == outsideModel){
+        modal.classList.remove("show-modal");
+    }
 });
